a68f9e32b3f2b4dd7a5b3fb49024969745a2439b
jesus m. rodriguez <jesusr@redhat.com>
2017-01-30 15:58:30 -0500

    add docker client support

--- pkg/ansibleapp/provision.go
+++ pkg/ansibleapp/provision.go
@@ -2,8 +2,29 @@ package ansibleapp
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/fsouza/go-dockerclient"
 	"github.com/op/go-logging"
 )
+
+// HACK: really need a better way to do docker run
+func runCommand(cmd string, args ...string) ([]byte, error) {
+	output, err := exec.Command(cmd, args...).CombinedOutput()
+	return output, err
+}
+
+func pullImage(client *docker.Client, spec *Spec) error {
+	// TODO: need to figure out where to send the output
+	client.PullImage(docker.PullImageOptions{Repository: spec.Name, OutputStream: os.Stdout}, docker.AuthConfiguration{})
+	return nil
+}
+
+func runImage(client *docker.Client, spec *Spec, parameters *Parameters) ([]byte, error) {
+	output, err := runCommand("docker", "run", spec.Name)
+	return output, err
+}
 
 // TODO: Figure out the right way to allow ansibleapp to log
 // It's passed in here, but that's a hard coupling point to
@@ -19,6 +40,22 @@ func Provision(spec *Spec, parameters *Parameters, log *logging.Logger) error {
 	log.Notice(fmt.Sprintf("Spec.Description: %s", spec.Description))
 	log.Notice(fmt.Sprintf("Parameters: %v", parameters))
 	log.Notice("============================================================")
+
+	endpoint := "unix:///var/run/docker.sock"
+	client, err := docker.NewClient(endpoint)
+	if err != nil {
+		log.Error("Could not load docker client")
+		return err
+	}
+
+	// pull image
+	pullImage(client, spec)
+	output, err := runImage(client, spec, parameters)
+	if err != nil {
+		log.Error("Problem running image")
+		return err
+	}
+	log.Info(string(output))
 
 	return nil
 }
