c562771d13afa8bb7db8b7e77cc85e965acf7cba
jesus m. rodriguez <jesusr@redhat.com>
2017-03-19 00:46:34 -0400

    test util file

---
+++ pkg/broker/util_test.go
@@ -0,0 +1,52 @@
+package broker
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/fusor/ansible-service-broker/pkg/ansibleapp"
+	ft "github.com/fusor/ansible-service-broker/pkg/fusortest"
+	"github.com/pborman/uuid"
+)
+
+func TestSpecToService(t *testing.T) {
+
+	param := []*ansibleapp.ParameterDescriptor{
+		&ansibleapp.ParameterDescriptor{
+			Name:        "hostport",
+			Description: "The host TCP port as the external end point",
+			Default:     float64(9001),
+			Type:        "foo",
+			Required:    true}}
+
+	spec := ansibleapp.Spec{
+		Id:          "50eb5637-6ffe-480d-a52e-a7e603a50fca",
+		Name:        "testspec",
+		Bindable:    false,
+		Description: "test spec to be converted",
+		Async:       "unsupported",
+		Parameters:  param}
+
+	descriptors := make(map[string]interface{})
+	descriptors["parameters"] = param
+
+	expectedsvc := Service{
+		ID:          uuid.Parse("50eb5637-6ffe-480d-a52e-a7e603a50fca"),
+		Name:        "testspec",
+		Description: "test spec to be converted",
+		Bindable:    false,
+		Plans:       nil,
+		Metadata:    descriptors,
+	}
+	svc := SpecToService(&spec)
+	ft.AssertEqual(t, svc.Name, expectedsvc.Name, "name is not equal")
+	ft.AssertEqual(t, svc.Description, expectedsvc.Description, "description is not equal")
+	ft.AssertEqual(t, svc.Bindable, expectedsvc.Bindable, "bindable wrong")
+}
+
+func TestProjectRoot(t *testing.T) {
+	gopath := os.Getenv("GOPATH")
+	rootpath := path.Join(gopath, "src/github.com/fusor/ansible-service-broker")
+	ft.AssertEqual(t, ProjectRoot(), rootpath, "paths not equal")
+}
