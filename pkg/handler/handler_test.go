0c76f6011cdaca80f0310e21b0df97881b1ac342
jesus m. rodriguez <jmrodri@gmail.com>
2017-03-20 22:15:57 -0400

    add a bunch of unit tests

--- pkg/handler/handler_test.go
+++ pkg/handler/handler_test.go
@@ -31,80 +31,149 @@ func (r TestRequest) Read(p []byte) (n int, err error) {
 */
 
 type MockBroker struct {
-	Name string
+	Name   string
+	Verify map[string]bool
+	Err    error
+}
+
+func (m *MockBroker) called(method string, called bool) {
+	if m.Verify == nil {
+		m.Verify = make(map[string]bool)
+	}
+	m.Verify[method] = called
 }
 
 func (m MockBroker) Bootstrap() (*broker.BootstrapResponse, error) {
-	return &broker.BootstrapResponse{10}, nil
+	m.called("bootstrap", true)
+	return &broker.BootstrapResponse{10}, m.Err
 }
 
 func (m MockBroker) Catalog() (*broker.CatalogResponse, error) {
-	return nil, nil
+	m.called("catalog", true)
+	return nil, m.Err
 }
 func (m MockBroker) Provision(uuid.UUID, *broker.ProvisionRequest) (*broker.ProvisionResponse, error) {
-	return nil, nil
+	m.called("provision", true)
+	return &broker.ProvisionResponse{Operation: "successful"}, m.Err
 }
 func (m MockBroker) Update(uuid.UUID, *broker.UpdateRequest) (*broker.UpdateResponse, error) {
-	return nil, nil
+	m.called("update", true)
+	return nil, m.Err
 }
 func (m MockBroker) Deprovision(uuid.UUID) (*broker.DeprovisionResponse, error) {
-	return nil, nil
+	m.called("deprovision", true)
+	return nil, m.Err
 }
 func (m MockBroker) Bind(uuid.UUID, uuid.UUID, *broker.BindRequest) (*broker.BindResponse, error) {
-	return nil, nil
+	m.called("bind", true)
+	return nil, m.Err
 }
 func (m MockBroker) Unbind(uuid.UUID, uuid.UUID) error {
-	return nil
+	m.called("unbind", true)
+	return m.Err
 }
 
 func TestNewHandler(t *testing.T) {
 
-	b := MockBroker{"testbroker"}
+	b := MockBroker{Name: "testbroker"}
 	handler := NewHandler(b)
 	ft.AssertNotNil(t, handler, "handler wasn't created")
 }
 
 func TestBootstrap(t *testing.T) {
 	// create handler for testing
-	b := MockBroker{"testbroker"}
+	b := MockBroker{Name: "testbroker"}
 	handler := handler{*mux.NewRouter(), b}
 	ft.AssertNotNil(t, handler, "")
 
 	trr := TestRequest{Msg: "hello world", done: true}
 	r := httptest.NewRequest("POST", "/v2/bootstrap", trr)
 	w := httptest.NewRecorder()
-	handler.bootstrap(w, r)
+	handler.bootstrap(w, r, nil)
 	ft.AssertEqual(t, w.Code, 200, "code not equal")
 }
 
 func TestCatalog(t *testing.T) {
 	// create handler for testing
-	b := MockBroker{"testbroker"}
+	b := MockBroker{Name: "testbroker"}
 	handler := handler{*mux.NewRouter(), b}
 	ft.AssertNotNil(t, handler, "")
 
 	trr := TestRequest{Msg: "hello world", done: true}
 	r := httptest.NewRequest("GET", "/v2/catalog", trr)
 	w := httptest.NewRecorder()
-	handler.catalog(w, r)
+	handler.catalog(w, r, nil)
 	ft.AssertEqual(t, w.Code, 200, "code not equal")
 }
 
-func TestProvision(t *testing.T) {
-	// skipping for now until we hash out the handler functions
+func TestProvisionCreate(t *testing.T) {
+	// create handler for testing
+	b := MockBroker{Name: "testbroker"}
+	handler := handler{*mux.NewRouter(), b}
+
+	trr := TestRequest{Msg: "{\"name\": \"hello world\"}"}
+	r := httptest.NewRequest("PUT", "/v2/service_instance/688eea24-9cf9-43e3-9942-d1863b2a16af", trr)
+	r.Header.Add("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	params := map[string]string{
+		"instance_uuid": "688eea24-9cf9-43e3-9942-d1863b2a16af",
+	}
+	handler.provision(w, r, params)
+	t.Log(w.Body)
+	ft.AssertEqual(t, w.Code, 201, "provision not created")
+}
+
+func TestProvisionInvalidUUID(t *testing.T) {
+	// create handler for testing
+	b := MockBroker{Name: "testbroker"}
+	handler := handler{*mux.NewRouter(), b}
+
+	r := httptest.NewRequest("PUT", "/v2/service_instance/invaliduuid", nil)
+	r.Header.Add("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	params := map[string]string{
+		"instance_uuid": "invaliduuid",
+	}
+	handler.provision(w, r, params)
+	ft.AssertEqual(t, w.Code, 400, "provision not created")
+	ft.AssertError(t, w.Body, "invalid instance_uuid")
+}
+
+func TestProvisionCouldnotReadRequest(t *testing.T) {
+	// create handler for testing
+	b := MockBroker{Name: "testbroker"}
+	handler := handler{*mux.NewRouter(), b}
+
+	r := httptest.NewRequest("PUT", "/v2/service_instance/invaliduuid", nil)
+	r.Header.Add("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	params := map[string]string{
+		"instance_uuid": "688eea24-9cf9-43e3-9942-d1863b2a16af",
+	}
+	handler.provision(w, r, params)
+	ft.AssertEqual(t, w.Code, 400, "provision not created")
+	ft.AssertError(t, w.Body, "could not read request: EOF")
+}
+
+func TestProvisionConflict(t *testing.T) {
 	t.SkipNow()
 	// create handler for testing
-	b := MockBroker{"testbroker"}
+	b := MockBroker{Name: "testbroker"}
 	handler := handler{*mux.NewRouter(), b}
-	handler.router.HandleFunc("/v2/service_instance/{instance_uuid}", handler.provision).Methods("PUT")
 
-	trr := TestRequest{Msg: "hello world", done: true}
-	r := httptest.NewRequest("PUT", "http://localhost:3000/v2/service_instance/688eea24-9cf9-43e3-9942-d1863b2a16af", trr)
+	trr := TestRequest{Msg: "{\"name\": \"hello world\"}"}
+	r := httptest.NewRequest("PUT", "/v2/service_instance/688eea24-9cf9-43e3-9942-d1863b2a16af", trr)
+	r.Header.Add("Content-Type", "application/json")
 	w := httptest.NewRecorder()
-	handler.ServeHTTP(w, r)
-	t.Log(w.Code)
-	t.Log(w.Result())
-	ft.AssertEqual(t, w.Code, 200, "code not equal")
+	params := map[string]string{
+		"instance_uuid": "688eea24-9cf9-43e3-9942-d1863b2a16af",
+	}
+	handler.provision(w, r, params)
+	t.Log(w.Body)
+	ft.AssertEqual(t, w.Code, 409, "provision not conflicted")
+	// TODO: need to figure out how to provide the proper error that satisfies
+	// IsAlreadyExists() on the handler.
+	ft.AssertError(t, w.Body, "could not read request: EOF")
 }
 
 func TestUpdate(t *testing.T) {
@@ -121,12 +190,12 @@ func TestUnbind(t *testing.T) {
 
 func TestBindInvalidInstance(t *testing.T) {
 	// create handler for testing
-	b := MockBroker{"testbroker"}
+	b := MockBroker{Name: "testbroker"}
 	handler := handler{*mux.NewRouter(), b}
 
 	trr := TestRequest{Msg: "hello world", done: true}
 	r := httptest.NewRequest("PUT", "/v2/service_instance/foo/service_bindings/bar", trr)
 	w := httptest.NewRecorder()
-	handler.bind(w, r)
+	handler.bind(w, r, nil)
 	ft.AssertEqual(t, w.Code, 400, "code not equal")
 }
