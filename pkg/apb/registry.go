bc5250048571ad1dfff3c45f8aa1ac53824e9636
jesus m. rodriguez <jesusr@redhat.com>
2017-03-22 16:15:01 -0400

    update comment

--- pkg/apb/registry.go
+++ pkg/apb/registry.go
@@ -11,7 +11,7 @@ type RegistryConfig struct {
 	Url  string
 	User string
 	Pass string
-	Org  string // Target org to load apbs from
+	Org  string // Target org to load playbookk bundles from
 }
 
 type Registry interface {
