ec06c5acdbcb5cb83f46bdd65b028cee92619566
Ryan Hallisey <rhallise@redhat.com>
2017-04-13 16:08:10 -0400

    Render the templates based on etc/dev.config.yaml
    Render templates with env vars specified in dev.config.yaml.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,44 @@ TEMPLATE_DIR="${PROJECT_ROOT}/templates"
 
 set -e
 
+# Based on https://gist.github.com/pkuczynski/8665367
+function parse_yaml() {
+    local prefix=$2
+    local s
+    local w
+    local fs
+    s='[[:space:]]*'
+    w='[a-zA-Z0-9_]*'
+    fs="$(echo @|tr @ '\034')"
+    sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" "$1" |
+    awk -F"$fs" '{
+    indent = length($1)/2;
+    vname[indent] = $2;
+    for (i in vname) {if (i > indent) {delete vname[i]}}
+        if (length($3) > 0) {
+            vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+            printf("%s%s%s=(\"%s\")\n", "'"$prefix"'",vn, $2, $3);
+        }
+    }' | sed 's/_=/+=/g'
+}
+
 function oc_create {
     oc create -f $TEMPLATE_DIR/$@
 }
 
-for tpl in services.yaml route.yaml etcd-deployment.yaml broker-deployment.yaml; do
+parse_yaml $PROJECT_ROOT/etc/dev.config.yaml > /tmp/dev-config
+sed -i "s/=(\"--\")//" /tmp/dev-config
+
+for tpl in services.yaml route.yaml etcd-deployment.yaml broker-deployment_template.yaml; do
+    if [ "${tpl}" == "broker-deployment_template.yaml" ]; then
+        cp $TEMPLATE_DIR/$tpl $TEMPLATE_DIR/broker-deployment.yaml
+        sed -i "s/{{dockerhub_pass}}/${registry_pass}/" $TEMPLATE_DIR/broker-deployment.yaml
+        sed -i "s/{{dockerhub_user}}/${registry_user}/" $TEMPLATE_DIR/broker-deployment.yaml
+        sed -i "s/{{openshift_pass}}/${openshift_pass}/" $TEMPLATE_DIR/broker-deployment.yaml
+        sed -i "s/{{openshift_target}}/${openshift_target}/" $TEMPLATE_DIR/broker-deployment.yaml
+        sed -i "s/{{openshift_user}}/${openshift_user}/" $TEMPLATE_DIR/broker-deployment.yaml
+    fi
+    source /tmp/dev-config
     oc_create $tpl
 done
