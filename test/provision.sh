4e476946b5de903aa6eca43aae63ffd36aa7ece5
jesus m. rodriguez <jmrodri@gmail.com>
2017-04-24 10:02:35 -0400

    some more test scripts

--- test/provision.sh
+++ test/provision.sh
@@ -1,11 +1,8 @@
 #!/bin/bash -e
 
-#instanceUUID="688eea24-9cf9-43e3-9942-d1863b2a16af"
-#planUUID="560789e6-d4fc-4bdf-b227-454002d5e7c6"
-#serviceUUID="f819ddd5-37d1-4698-b3fb-a3cc99a35d2e" # ghost-ansibleapp
-instanceUUID="688eea24-9cf9-43e3-9942-d1863b2a16af"
+instanceUUID="abd21149-07d7-4a8a-b40b-4b815110c3cc"
 planUUID="4c10ff42-be89-420a-9bab-27a9bef9aed8"
-serviceUUID="84e8173e-5ced-4489-9b4d-aac7e779c47e"
+serviceUUID="0aaafc10-132b-41a8-a58c-73268ff1006a"
 
 req="{
   \"plan_id\": \"$planUUID\",
