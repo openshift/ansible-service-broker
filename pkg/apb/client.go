277f6124fe4f68a7b2fb946be2f2667cd7b021dd
Ryan Hallisey <rhallise@redhat.com>
2017-05-16 17:32:23 -0400

    Build a Kubernetes client object based off of the local config
    The client object is created from $HOME/.kube/config.

--- pkg/apb/client.go
+++ pkg/apb/client.go
@@ -7,7 +7,12 @@ import (
 
 	docker "github.com/fsouza/go-dockerclient"
 	logging "github.com/op/go-logging"
+	restclient "k8s.io/client-go/rest"
+
 	"github.com/pborman/uuid"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"k8s.io/kubernetes/pkg/client/clientset_generated/clientset"
 )
 
 /*
@@ -41,8 +46,24 @@ type ClusterConfig struct {
 }
 
 type Client struct {
-	dockerClient *docker.Client
-	log          *logging.Logger
+	dockerClient  *docker.Client
+	ClusterClient *clientset.Clientset
+	RESTClient    restclient.Interface
+	log           *logging.Logger
+}
+
+func createClientConfigFromFile(configPath string) (*restclient.Config, error) {
+	// TODO: Better error handling here. An incorrect path leads to a seg fault
+	clientConfig, err := clientcmd.LoadFromFile(configPath)
+	if err != nil {
+		return nil, fmt.Errorf("error while loading config from file %v: %v", configPath, err)
+	}
+
+	config, err := clientcmd.NewDefaultClientConfig(*clientConfig, &clientcmd.ConfigOverrides{}).ClientConfig()
+	if err != nil {
+		return nil, fmt.Errorf("error while creating kubeconfig: %v", err)
+	}
+	return config, nil
 }
 
 func NewClient(log *logging.Logger) (*Client, error) {
@@ -52,9 +73,24 @@ func NewClient(log *logging.Logger) (*Client, error) {
 		return nil, err
 	}
 
+	clientConfig, err := createClientConfigFromFile(homedir.HomeDir() + "/.kube/config")
+
+	// TODO: Add code for an internalclientset. Kubernetes generates config
+	// files and loads them into every pod allowing connection to the
+	// APIserver from inside the cluster. Since the broker will primarily
+	// be run in the cluster, we need to use that configfile.
+	clientset, err := clientset.NewForConfig(clientConfig)
+	if err != nil {
+		log.Warning("Failed to create a ClientSet: %v.", err)
+	}
+
+	rest := clientset.Core().RESTClient()
+
 	client := &Client{
-		dockerClient: dockerClient,
-		log:          log,
+		dockerClient:  dockerClient,
+		ClusterClient: clientset,
+		RESTClient:    rest,
+		log:           log,
 	}
 
 	return client, nil
