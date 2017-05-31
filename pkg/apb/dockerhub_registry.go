74ee7831412ac82452a4a58eb3d675e4e16206af
Shawn Hurley <shurley@redhat.com>
2017-05-31 11:56:05 -0400

    adding ability to get all the dockerhub images from an org.

--- pkg/apb/dockerhub_registry.go
+++ pkg/apb/dockerhub_registry.go
@@ -2,6 +2,7 @@ package apb
 
 import (
 	"bytes"
+	"context"
 	b64 "encoding/base64"
 	"encoding/json"
 	"errors"
@@ -116,6 +117,7 @@ func (r *DockerHubRegistry) loadBundleImageData(org string) ([]*ImageData, error
 	type ImageResponse struct {
 		Count   int       `json:"count"`
 		Results []*Images `json:"results"`
+		Next    string    `json:"next"`
 	}
 
 	data := Payload{
@@ -152,6 +154,7 @@ func (r *DockerHubRegistry) loadBundleImageData(org string) ([]*ImageData, error
 	// through the entire repo instead of being maxed at 100.
 	// (Example) rhallisey has > 100 images in his repo and the limiting
 	// page size prevents new APBs from appearing in the broker.
+
 	req, err = http.NewRequest("GET", os.ExpandEnv(fmt.Sprintf("https://hub.docker.com/v2/repositories/%v/?page_size=100", org)), nil)
 	if err != nil {
 		return nil, err
@@ -173,20 +176,74 @@ func (r *DockerHubRegistry) loadBundleImageData(org string) ([]*ImageData, error
 	}
 
 	channel := make(chan *ImageData)
-
-	for _, imageName := range imageResp.Results {
-		r.log.Debug("Trying to load " + imageName.Namespace + "/" + imageName.Name)
-		go r.loadImageData("docker://"+imageName.Namespace+"/"+imageName.Name, channel)
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	count2 := 0
+	loadImagesFromResult := func(ctx context.Context, images []*Images) {
+		for _, imageName := range images {
+			r.log.Debugf("Trying to load %v/%v - %v", imageName.Namespace, imageName.Name, count2)
+			count2++
+			go r.loadImageData(ctx, "docker://"+imageName.Namespace+"/"+imageName.Name, channel)
+		}
 	}
 
+	var getNextImages func(ctx context.Context, org, token, url string, ch chan<- *ImageData, cancelFunc context.CancelFunc)
+
+	getNextImages = func(ctx context.Context, org, token, url string, ch chan<- *ImageData, cancelFunc context.CancelFunc) {
+		r.log.Notice("get next images")
+		req, err = http.NewRequest("GET", url, nil)
+		if err != nil {
+			r.log.Errorf("unable to get next images for url: %v - %v", url, err)
+			cancelFunc()
+		}
+		req.Header.Set("Authorization", fmt.Sprintf("JWT %v", token))
+
+		resp, err = http.DefaultClient.Do(req)
+		if err != nil {
+			r.log.Errorf("unable to get next images for url: %v - %v", url, err)
+			cancelFunc()
+		}
+		defer resp.Body.Close()
+
+		imageList, err := ioutil.ReadAll(resp.Body)
+
+		iResp := ImageResponse{}
+		err = json.Unmarshal(imageList, &iResp)
+		if err != nil {
+			r.log.Errorf("unable to get next images for url: %v - %v", url, err)
+			cancelFunc()
+		}
+		r.log.Notice("get next images %#v", iResp)
+		//Keep getting the images
+		if iResp.Next != "" {
+			r.log.Notice("getting next page of results")
+			go getNextImages(ctx, org, token, imageResp.Next, ch, cancelFunc)
+		}
+		loadImagesFromResult(ctx, iResp.Results)
+	}
+
+	if imageResp.Next != "" {
+		go getNextImages(ctx, org, string(tokenResp.Token), imageResp.Next, channel, cancelFunc)
+	}
+	loadImagesFromResult(ctx, imageResp.Results)
+	//If no results in the fist call then close the channel as nothing will get loaded.
+	if len(imageResp.Results) == 0 {
+		r.log.Info("canceled retrieval as not items in org")
+		cancelFunc()
+	}
 	var apbData []*ImageData
 	counter := 1
 	for imageData := range channel {
+		select {
+		case <-ctx.Done():
+			r.log.Infof("unable to get images from data as channel was canceled - %v", ctx.Err())
+			close(channel)
+			continue
+		default:
+		}
+		r.log.Notice("%v", counter)
 		if imageData.Error != nil {
 			r.log.Error("Something went wrong loading img data for [ %s ]", imageData.Name)
 			r.log.Error(fmt.Sprintf("Error: %s", imageData.Error))
-			counter++
-			continue
 		}
 
 		if imageData.IsPlaybookBundle {
@@ -196,7 +253,7 @@ func (r *DockerHubRegistry) loadBundleImageData(org string) ([]*ImageData, error
 			r.log.Notice("We did NOT add the imageData for some reason")
 		}
 
-		if counter != len(imageResp.Results) {
+		if counter < imageResp.Count {
 			counter++
 		} else {
 			close(channel)
@@ -211,7 +268,7 @@ func (r *DockerHubRegistry) loadBundleImageData(org string) ([]*ImageData, error
 	return apbData, nil
 }
 
-func (r *DockerHubRegistry) loadImageData(imageName string, channel chan<- *ImageData) {
+func (r *DockerHubRegistry) loadImageData(ctx context.Context, imageName string, channel chan<- *ImageData) {
 	// TODO: Error handling!
 	img, err := parseImage(imageName)
 	if err != nil {
@@ -240,7 +297,11 @@ func (r *DockerHubRegistry) loadImageData(imageName string, channel chan<- *Imag
 		channel <- &outputData
 	} else {
 		outputData.IsPlaybookBundle = false
-		channel <- &outputData
+	}
+	select {
+	case channel <- &outputData:
+	case <-ctx.Done():
+		r.log.Errorf("loading images failed due to context err - %v", ctx.Err())
 	}
 }
 
