50b302255dac0e9951da610f7186088888f19da9
John Matthews <jwmatthews@gmail.com>
2017-06-08 15:53:30 -0400

    Added an OpenShift template for deploying broker & etcd

---
+++ templates/run_template.sh
@@ -0,0 +1,24 @@
+PROJECT="ansible-service-broker"
+BROKER_IMAGE="ansibleplaybookbundle/ansible-service-broker:latest"
+OPENSHIFT_TARGET="https://kubernetes.default"
+OPENSHIFT_USER="admin"
+OPENSHIFT_PASS="admin"
+DOCKERHUB_USER="CHANGEME"
+DOCKERHUB_PASS="CHANGEME"
+DOCKERHUB_ORG="ansibleplaybookbundle"
+
+
+VARS="-p BROKER_IMAGE=${BROKER_IMAGE} -p OPENSHIFT_TARGET=${OPENSHIFT_TARGET} -p OPENSHIFT_PASS=${OPENSHIFT_PASS} -p OPENSHIFT_USER=${OPENSHIFT_USER} -p DOCKERHUB_ORG=${DOCKERHUB_ORG} -p DOCKERHUB_PASS=${DOCKERHUB_PASS} -p DOCKERHUB_USER=${DOCKERHUB_USER}"
+
+oc delete project ${PROJECT}
+oc projects | grep ${PROJECT}
+while [ $? -eq 0 ]
+do
+  echo "Waiting for ${PROJECT} to be deleted"
+  sleep 5;
+  oc projects | grep ${PROJECT}
+done
+
+
+oc new-project ${PROJECT}
+oc process -f deploy-ansible-service-broker.template.yaml -n ${PROJECT} ${VARS}  | oc create -f -
