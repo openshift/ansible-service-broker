33a1e0c61c83b392a7ab382eb0cc8780058eea75
Fabian von Feilitzsch <fabian@fabianism.us>
2017-06-21 15:19:10 -0400

    ASB_CONF_FILE -> BROKER_CONFIG

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -5,11 +5,12 @@ if [ ${USER_UID} != ${USER_ID} ]; then
   sed "s@${USER_NAME}:x:\${USER_ID}:@${USER_NAME}:x:${USER_ID}:@g" ${BASE_DIR}/etc/passwd.template > /etc/passwd
 fi
 
-ASB_CONF_FILE=${ASB_CONF_FILE:-/etc/ansible-service-broker/broker-config.yaml}
+BROKER_CONFIG=${BROKER_CONFIG:-/etc/ansible-service-broker/broker-config.yaml}
 
-if [ ! -f "$ASB_CONF_FILE" ] ; then
-  echo "No config file mounted to $ASB_CONF_FILE"
+if [ ! -f "$BROKER_CONFIG" ] ; then
+  echo "No config file mounted to $BROKER_CONFIG"
+  exit 1
 fi
-echo "Using config file mounted to $ASB_CONF_FILE"
+echo "Using config file mounted to $BROKER_CONFIG"
 
-ansible-service-broker -c $ASB_CONF_FILE
+ansible-service-broker -c $BROKER_CONFIG
