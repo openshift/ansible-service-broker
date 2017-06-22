0e558075b6714fc5c9f7bbef1a6ddce72c0a9155
Fabian von Feilitzsch <fabianvf@users.noreply.github.com>
2017-06-21 20:52:47 -0400

    Expect a config file to be mounted  (#211)
    * Expect a config file to be mounted rather than create one with sed based on environment variables
    
    * ASB_CONF_FILE -> BROKER_CONFIG
    
    * update template parameter

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -5,33 +5,12 @@ if [ ${USER_UID} != ${USER_ID} ]; then
   sed "s@${USER_NAME}:x:\${USER_ID}:@${USER_NAME}:x:${USER_ID}:@g" ${BASE_DIR}/etc/passwd.template > /etc/passwd
 fi
 
-ASB_CONF=/etc/ansible-service-broker/broker-config.yaml
+BROKER_CONFIG=${BROKER_CONFIG:-/etc/ansible-service-broker/broker-config.yaml}
 
-if [[ -z "${DOCKERHUB_USER}" ]] || [[ -z "${DOCKERHUB_PASS}" ]] || [[ -z "${DOCKERHUB_ORG}" ]]; then
-  echo "ERROR: \$DOCKERHUB_USER and \$DOCKERHUB_PASS environment vars must be defined!"
-  echo "These are required bootstrapping ansibleapp metadata from Dockerhub"
-  echo "Vars can be set with docker run -e. Ex: -e=\"DOCKERHUB_USER=eriknelson\""
+if [ ! -f "$BROKER_CONFIG" ] ; then
+  echo "No config file mounted to $BROKER_CONFIG"
   exit 1
-else
-  echo "Got DOCKERHUB credentials."
-  sed -i "s|{{DOCKERHUB_USER}}|${DOCKERHUB_USER}|" $ASB_CONF
-  sed -i "s|{{DOCKERHUB_PASS}}|${DOCKERHUB_PASS}|" $ASB_CONF
-  sed -i "s|{{DOCKERHUB_ORG}}|${DOCKERHUB_ORG}|" $ASB_CONF
 fi
+echo "Using config file mounted to $BROKER_CONFIG"
 
-if [[ -z "${OPENSHIFT_TARGET}" ]] || [[ -z "${OPENSHIFT_USER}" ]] || [[ -z "${OPENSHIFT_PASS}" ]]; then
-  echo "Openshift cluster credentials not provided. Assuming the broker is running inside an Openshift cluster"
-  sed -i "s/openshift:.*/openshift: {}/" $ASB_CONF
-  sed -i "s/  target: {{OPENSHIFT_TARGET}}//" $ASB_CONF
-  sed -i "s/  user: {{OPENSHIFT_USER}}//" $ASB_CONF
-  sed -i "s/  pass: {{OPENSHIFT_PASS}}//" $ASB_CONF
-else
-  echo "Got OPENSHIFT credentials."
-  sed -i "s|{{OPENSHIFT_TARGET}}|${OPENSHIFT_TARGET}|" $ASB_CONF
-  sed -i "s|{{OPENSHIFT_USER}}|${OPENSHIFT_USER}|" $ASB_CONF
-  sed -i "s|{{OPENSHIFT_PASS}}|${OPENSHIFT_PASS}|" $ASB_CONF
-fi
-
-echo $ASB_CONF
-
-ansible-service-broker
+ansible-service-broker -c $BROKER_CONFIG
