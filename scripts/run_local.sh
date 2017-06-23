b69d2a5a660c913fc342b6865b351a75ff126812
Jesus Rodriguez <jmrodri@gmail.com>
2017-06-23 14:39:20 -0400

    remove trailing whitespace (#226)

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 MY_VARS="./my_local_dev_vars"
-if [ ! -f $MY_VARS ]; then 
+if [ ! -f $MY_VARS ]; then
   echo "Please create $MY_VARS"
   echo "cp $MY_VARS.example $MY_VARS"
   echo "then edit as needed"
   exit 1
-fi 
+fi
 
 source ./${MY_VARS}
 if [ "$?" -ne "0" ]; then
   echo "Error reading in ${MY_VARS}"
   exit 1
-fi 
+fi
 
-if [ -z "${BROKER_CMD}" ]; then 
+if [ -z "${BROKER_CMD}" ]; then
   echo "Please ensure BROKER_CMD is defined in ${MY_VARS}"
-  exit 1 
-fi 
+  exit 1
+fi
 
 export KUBERNETES_SERVICE_HOST=${OPENSHIFT_SERVER_HOST}
 export KUBERNETES_SERVICE_PORT=${OPENSHIFT_SERVER_PORT}
@@ -24,12 +24,12 @@ export KUBERNETES_SERVICE_PORT=${OPENSHIFT_SERVER_PORT}
 BROKER_CONFIG=${GENERATED_BROKER_CONFIG}
 if [ ! -z "$1" ]; then
   BROKER_CONFIG="$1"
-fi 
+fi
 
-if [ -z "${BROKER_CONFIG}" ]; then 
+if [ -z "${BROKER_CONFIG}" ]; then
   echo "Please specify a broker configuration file to run"
   exit 1
-fi 
+fi
 
 echo "Running ${BROKER_CMD} --config ${BROKER_CONFIG}"
 ${BROKER_CMD} --config ${BROKER_CONFIG}
