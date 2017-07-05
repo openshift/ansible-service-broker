024ab3667face28e56640a2f8006250c2ab4baf7
Dylan Murray <dymurray@redhat.com>
2017-07-05 11:50:37 -0400

    1467905 - Added error handling for images with improper APB spec

--- pkg/apb/types.go
+++ pkg/apb/types.go
@@ -114,6 +114,9 @@ func specsLogDump(specs []*Spec, log *logging.Logger) {
 func NewSpecManifest(specs []*Spec) SpecManifest {
 	manifest := make(map[string]*Spec)
 	for _, spec := range specs {
+		if spec == nil {
+			return nil
+		}
 		manifest[spec.Id] = spec
 	}
 	return manifest
