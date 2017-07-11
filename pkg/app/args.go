4d70494c3c603a2c1e11ae0c1ae79bb96e4f2f2e
Ryan Hallisey <rhallise@redhat.com>
2017-07-11 12:10:58 -0400

    1469401 - Print out error messages only once (#264)
    We are printing error messages and the help menu
    twice. Also, clean up some unused options.

--- pkg/app/args.go
+++ pkg/app/args.go
@@ -1,9 +1,6 @@
 package app
 
 import (
-	"errors"
-	"fmt"
-
 	flags "github.com/jessevdk/go-flags"
 )
 
@@ -20,24 +17,5 @@ func CreateArgs() (Args, error) {
 		return args, err
 	}
 
-	err = validateArgs(&args)
-	if err != nil {
-		return args, err
-	}
-
 	return args, nil
-}
-
-func validateArgs(args *Args) error {
-	var err error
-	if args.ConfigFile == "" {
-		err = errors.New("must provide a config file location with -c, or --config")
-	}
-
-	return err
-}
-
-func ArgsUsage() {
-	// TODO
-	fmt.Println("USAGE: To be implemented...")
 }
