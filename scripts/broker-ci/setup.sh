3e396987f64388904b0c6fd3ef297c35f83b81dc
Ryan Hallisey <rhallise@redhat.com>
2017-08-09 10:13:08 -0400

    Prevent CI failures on building the broker
    Travis occasionally fails when building the broker image.
    Add a retry around the build.

--- scripts/broker-ci/setup.sh
+++ scripts/broker-ci/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+BROKER_DIR="$(dirname "${BASH_SOURCE}")/../.."
+source "${BROKER_DIR}/scripts/broker-ci/error.sh"
+
+BUILD_ERROR=false
+MAKE_DEPLOY_ERROR=false
+CLUSTER_SETUP_ERROR=false
+
 set -ex
 
 function cluster-setup () {
@@ -13,8 +20,10 @@ dockerhub_user_password: brokerciuser
 EOF
 
     pushd catasb/local/gate/
-    ./run_gate.sh
+    ./run_gate.sh || CLUSTER_SETUP_ERROR=true
     popd
+
+    env-error-check "cluster-setup"
 
     cat <<EOF > "scripts/my_local_dev_vars"
 OPENSHIFT_SERVER_HOST=172.17.0.1
@@ -35,12 +44,38 @@ IMAGE_PULL_POLICY="Always"
 EOF
 }
 
+function make-build-image {
+    set +x
+    RETRIES=3
+    for x in $(seq $RETRIES); do
+	make build-image
+	if [ $? -eq 0 ]; then
+	    print-with-green "Broker container completed building."
+	    break
+	else
+	    print-with-yellow "Broker container failed to build."
+	    print-with-yellow "Retrying..."
+	fi
+    done
+    if [ "${x}" -eq "${RETRIES}" ]; then
+	print-with-red "Broker container failed to build."
+	BUILD_ERROR=true
+    fi
+    env-error-check "make-build-image"
+    set -x
+}
+
+function make-deploy {
+    make deploy
+    NAMESPACE="ansible-service-broker" wait-for-resource pod create asb
+    env-error-check "make-deploy"
+}
+
 function local-env() {
     oc login --insecure-skip-tls-verify 172.17.0.1:8443 -u admin -p admin
     oc project default
-    make build-image
-    make deploy
-    sleep 15
+    make-build-image
+    make-deploy
     oc create -f ./scripts/broker-ci/broker-resource.yaml
 }
 
