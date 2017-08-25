e22850adb8acace546ac87def23a2e3909ef8231
Shawn Hurley <shurley@redhat.com>
2017-08-25 13:51:58 -0400

    adding unit test for addNameAndIDForSpec

--- pkg/broker/broker_test.go
+++ pkg/broker/broker_test.go
@@ -32,3 +32,12 @@ func TestUpdate(t *testing.T) {
 	}
 	ft.AssertEqual(t, err, notImplemented, "Update must have been implemented")
 }
+
+func TestAddNameAndIDForSpecStripsTailingDash(t *testing.T) {
+	spec1 := apb.Spec{Image: "1234567890123456789012345678901234567890-"}
+	spec2 := apb.Spec{Image: "org/hello-world-apb"}
+	spcs := []*apb.Spec{&spec1, &spec2}
+	addNameAndIDForSpec(spcs, "h")
+	ft.AssertEqual(t, "h-1234567890123456789012345678901234567890", spcs[0].FQName)
+	ft.AssertEqual(t, "h-org-hello-world-apb", spcs[1].FQName)
+}
