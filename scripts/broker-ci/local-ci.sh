8d747a67c929d34233e46614cc8e42666f59bb2d
Ryan Hallisey <rhallise@redhat.com>
2017-09-26 09:46:08 -0400

    Increase the sleep some more and add more debugging

--- scripts/broker-ci/local-ci.sh
+++ scripts/broker-ci/local-ci.sh
@@ -59,14 +59,26 @@ function unbind {
 function bind-credential-check {
     set +x
     print-with-green "Waiting for the Bind to be created"
+    oc describe pods -n default $(oc get pods -n default | grep mediawiki | grep Terminating | awk '{ print $1 }' | head -1)
+    sleep 10
+
+    oc describe pods -n default $(oc get pods -n default | grep mediawiki | grep Terminating | awk '{ print $1 }' | head -1)
+    oc get pods -n default
+    sleep 10
+
+    oc describe pods -n default $(oc get pods -n default | grep mediawiki | grep Terminating | awk '{ print $1 }' | head -1)
+    sleep 10
+
+    oc describe pods -n default $(oc get pods -n default | grep mediawiki | grep Terminating | awk '{ print $1 }' | head -1)
     sleep 10
 
     RETRIES=10
     for x in $(seq $RETRIES); do
+	oc get pods -n default
+	oc describe pods -n default $(oc get pods -n default | grep mediawiki | grep Terminating | awk '{ print $1 }' | head -1)
+
 	oc delete pods $(oc get pods -o name -l app=mediawiki123 -n default | head -1 | cut -f 2 -d '/') -n default  --force --grace-period=10 || BIND_ERROR=true
 	./scripts/broker-ci/wait-for-resource.sh create pod mediawiki >> /tmp/wait-for-pods-log 2>&1
-
-	oc describe pods -n default $(oc get pods -n default | grep mediawiki | grep Terminating | awk '{ print $1 }' | head -1)
 
 	# Filter for 'podpreset.admission.kubernetes.io' in the pod
 	preset_test=$(oc get pods $(oc get pods -n default | grep mediawiki | grep Running | awk $'{ print $1 }') -o yaml -n default | grep podpreset | awk $'{ print $1}' | cut -f 1 -d '/')
