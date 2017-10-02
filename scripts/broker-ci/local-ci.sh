34d27c6d14bfba6c5d854508cbcfc909e5da7b1c
Ryan Hallisey <rhallise@redhat.com>
2017-10-02 13:20:37 -0400

    Use the latest broker-ci repo instead of a release for now

--- scripts/broker-ci/local-ci.sh
+++ scripts/broker-ci/local-ci.sh
@@ -3,6 +3,13 @@
 ######
 # Main
 ######
-pushd vendor/github.com/rthallisey/service-broker-ci
+
+# Test with latest code first. Then, we'll move to a release branch
+git clone https://github.com/rthallisey/service-broker-ci
+pushd service-broker-ci
 make run
 popd
+
+# pushd vendor/github.com/rthallisey/service-broker-ci
+# make run
+# popd
