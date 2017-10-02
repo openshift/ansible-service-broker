2854946edd9e279d0471183e3c47ee96b7c034b4
Ryan Hallisey <rhallise@redhat.com>
2017-10-02 11:46:49 -0400

    Pass in args to the deploy scripts
    The gate showed a false positive on #434, this will fix it.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 BROKER_ROOT=$(dirname "${BASH_SOURCE}")/..
+ARGS="${@}"
 
 if [[ "${KUBERNETES}" ]]; then
     echo "Using Kubernetes"
-    "${BROKER_ROOT}/scripts/kubernetes/deploy.sh"
+    "${BROKER_ROOT}/scripts/kubernetes/deploy.sh" $ARGS
 else
     echo "Using OpenShift"
-    "${BROKER_ROOT}/scripts/openshift/deploy.sh"
+    "${BROKER_ROOT}/scripts/openshift/deploy.sh" $ARGS
 fi
