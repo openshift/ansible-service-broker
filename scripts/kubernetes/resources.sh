3b3c4113c7e05d70c6438b8bd465c7c6f3258c0d
Ryan Hallisey <rhallise@redhat.com>
2017-10-02 10:56:47 -0400

    Make the prep_local_devel_env script work for Kubernetes & Openshift (#434)
    * Make the prep_local_devel_env script work for Kubernetes & Openshift
    
    Most of the prep_local_devel_env.sh script was shared between the
    two clusters. The main differences were:
      - etcd_port: 2379
      - kubernetes_port: 6443
      - routes
      - deployments
      - ca.crt vs service-ca.crt
    
    * Create the broker endpoint separately
    
    If you create endpoint 'asb', it will be associated with
    the service 'asb'. That service, doesn't connect to a running
    pod so the endpoint gets no ip address. Instead, create another
    endpoint that connect to the service on the same ip:port to use
    as the endpoint to contact the broker.
    
    * Reorganize scirpts
    
    * Now that we create asb-1338, make sure we delete it
    
    * Never commited the k8s-local-dev-changes template
    
    * Remove empty script
    
    * Remove cluster-redirect script

---
+++ scripts/kubernetes/resources.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+function cluster::deployments {
+    action="$1"
+    name="$2"
+    args="${@:3}"
+
+    kubectl $action deployments $name $args
+}
+
+function cluster::routes {
+    action="$1"
+    name="$2"
+    args="${@:3}"
+
+    if [ "${action}" == "delete" ]; then
+	echo "Kubernetes doesn't need to delete a route"
+    elif [[ "${args}" == *"jsonpath"* ]]; then
+	for r in $(seq 10); do
+	    endpoint="$(kubectl get endpoints | grep etcd | awk '{ print $2 }' | cut -f 1 -d ':')"
+	    if [ "${endpoint}" != "<none>" ]; then
+		echo "${endpoint}"
+		break
+	    fi
+	    sleep 1
+	done
+    else
+	kubectl $action endpoints $name $args
+    fi
+}
+
+function cluster::process {
+    "${TEMPLATE_DIR}/k8s-template.py"
+    kubectl create -f "${TEMPLATE_DIR}/k8s-local-dev-changes.yaml"
+
+    cat <<EOF | kubectl create -f -
+apiVersion: v1
+kind: Endpoints
+metadata:
+  labels:
+    app: ansible-service-broker
+    service: asb
+  name: asb-1338
+subsets:
+- addresses:
+  - ip: 10.32.1.128
+  ports:
+  - name: port-1338
+    port: 1338
+    protocol: TCP
+EOF
+}
+
+function cluster::get-ca {
+    kubectl get secret ${BROKER_SVC_ACCT_SECRET_NAME} -n ${ASB_PROJECT} -o jsonpath='{ .data.ca\.crt }'
+}
+
+function cluster::etcd-port {
+    kubectl get endpoints | grep etcd | awk '{ print $2 }' | cut -f 2 -d ':'
+}
--- scripts/prep_local_devel_env.sh
+++ scripts/prep_local_devel_env.sh
@@ -20,8 +20,20 @@ BROKER_SVC_ACCT_NAME="asb"
 BROKER_SVC_ACCT="system:serviceaccount:${ASB_PROJECT}:${BROKER_SVC_ACCT_NAME}"
 
 # Faking out https://github.com/kubernetes/client-go/blob/master/rest/config.go#L309
-export KUBERNETES_SERVICE_HOST=${OPENSHIFT_SERVER_HOST}
-export KUBERNETES_SERVICE_PORT=${OPENSHIFT_SERVER_PORT}
+export KUBERNETES_SERVICE_HOST=${CLUSTER_HOST}
+export KUBERNETES_SERVICE_PORT=${CLUSTER_PORT}
+if [ -n "${KUBERNETES}" ]; then
+    echo "Kubernetes Cluster"
+    if [ "${KUBERNETES_SERVICE_PORT}" == "8443" ]; then
+	echo "ERROR: Kubernetes uses port 6443, not ${KUBERNETES_SERVICE_PORT}"
+	exit 1
+    fi
+    source "${SCRIPT_DIR}/kubernetes/resources.sh"
+else
+    echo "OpenShift Cluster"
+    source "${SCRIPT_DIR}/openshift/resources.sh"
+fi
+
 SVC_ACCT_TOKEN_DIR=/var/run/secrets/kubernetes.io/serviceaccount
 SVC_ACCT_CA_CRT=$SVC_ACCT_TOKEN_DIR/ca.crt
 SVC_ACCT_TOKEN_FILE=$SVC_ACCT_TOKEN_DIR/token
@@ -46,13 +58,13 @@ fi
 
 
 # Determine the name of the secret which has the 'asb' service account info
-BROKER_SVC_ACCT_SECRET_NAME=`oc get serviceaccount asb -n ansible-service-broker -o jsonpath='{.secrets[0].name}'`
+BROKER_SVC_ACCT_SECRET_NAME=`kubectl get serviceaccount asb -n ansible-service-broker -o jsonpath='{.secrets[0].name}'`
 echo "Broker Service Account Token is in secret: ${BROKER_SVC_ACCT_SECRET_NAME}"
 
 ###
 # Fetch the service-ca.crt for the service account
 ###
-SVC_ACCT_CA_CRT_DATA=`oc get secret ${BROKER_SVC_ACCT_SECRET_NAME} -n ${ASB_PROJECT} -o jsonpath='{ .data.service-ca\.crt }'`
+SVC_ACCT_CA_CRT_DATA="$(cluster::get-ca)"
 # Base64 Decode
 SVC_ACCT_CA_CRT_DATA=`echo ${SVC_ACCT_CA_CRT_DATA} | base64 --decode `
 if [ "$?" -ne 0 ]; then
@@ -75,7 +87,7 @@ if [ ! -d $SVC_ACCT_TOKEN_DIR ]; then
   echo "Ensure your user can write to it."
   exit 1
 fi
-BROKER_SVC_ACCT_TOKEN=`oc get secret ${BROKER_SVC_ACCT_SECRET_NAME} -n ${ASB_PROJECT} -o jsonpath='{ .data.token }'`
+BROKER_SVC_ACCT_TOKEN=`kubectl get secret ${BROKER_SVC_ACCT_SECRET_NAME} -n ${ASB_PROJECT} -o jsonpath='{ .data.token }'`
 BROKER_SVC_ACCT_TOKEN=`echo ${BROKER_SVC_ACCT_TOKEN} | base64 --decode`
 ###
 # Note:
@@ -93,7 +105,7 @@ echo -e "Wrote \n${BROKER_SVC_ACCT_TOKEN}\n to: ${SVC_ACCT_TOKEN_FILE}\n"
 ###
 # Fetch the tls.crt for the asb deployment
 ###
-TLS_CRT_DATA=`oc get secret -n ${ASB_PROJECT} asb-tls -o jsonpath='{ .data.tls\.crt }'`
+TLS_CRT_DATA=`kubectl get secret -n ${ASB_PROJECT} asb-tls -o jsonpath='{ .data.tls\.crt }'`
 # Remove quotes from variable
 TLS_CRT_DATA=( $(eval echo ${TLS_CRT_DATA[@]}) )
 # Base64 Decode
@@ -113,7 +125,7 @@ echo -e "Wrote \n${TLS_CRT_DATA}\n to: ${TLS_CRT}\n"
 ###
 # Fetch the tls.crt for the asb deployment
 ###
-TLS_KEY_DATA=`oc get secret -n ${ASB_PROJECT} asb-tls -o jsonpath='{ .data.tls\.key }'`
+TLS_KEY_DATA=`kubectl get secret -n ${ASB_PROJECT} asb-tls -o jsonpath='{ .data.tls\.key }'`
 # Remove quotes from variable
 TLS_KEY_DATA=( $(eval echo ${TLS_KEY_DATA[@]}) )
 # Base64 Decode
@@ -130,33 +142,35 @@ fi
 echo "TLS Cert: tls.crt"
 echo -e "Wrote \n${TLS_KEY_DATA}\n to: ${TLS_KEY}\n"
 # Kill any running broker pods
-oc scale deploymentconfig asb --replicas 0 -n ${ASB_PROJECT}
+cluster::deployments scale asb --replicas 0 -n ${ASB_PROJECT}
 # Wait for asb pod to be destroyed
-oc get pods -n ${ASB_PROJECT} | grep asb
+kubectl get pods -n ${ASB_PROJECT} | grep asb
 while [ "$?" -ne 1 ]; do
   echo "Waiting for asb deployment to scale down"
   sleep 5
-  oc get pods -n ${ASB_PROJECT} | grep asb
+  kubectl get pods -n ${ASB_PROJECT} | grep asb
 done
 
-oc scale deployment etcd --replicas 0 -n ${ASB_PROJECT}
+kubectl scale deployment etcd --replicas 0 -n ${ASB_PROJECT}
 # Wait for asb pod to be destroyed
-oc get pods -n ${ASB_PROJECT} | grep etcd
+kubectl get pods -n ${ASB_PROJECT} | grep etcd
 while [ "$?" -ne 1 ]; do
   echo "Waiting for etcd deployment to scale down"
   sleep 5
-  oc get pods -n ${ASB_PROJECT} | grep etcd
+  kubectl get pods -n ${ASB_PROJECT} | grep etcd
 done
 
 TERMINATION="reencrypt"
 
-oc delete deployment etcd -n ${ASB_PROJECT}
-oc delete endpoints asb -n ${ASB_PROJECT}
-oc delete service asb  -n ${ASB_PROJECT}
-oc delete route asb-etcd -n ${ASB_PROJECT}
-oc delete service etcd -n ${ASB_PROJECT}
+kubectl delete deployment etcd -n ${ASB_PROJECT}
+kubectl delete endpoints asb -n ${ASB_PROJECT}
+kubectl delete endpoints asb-1338 -n ${ASB_PROJECT} --ignore-not-found
+kubectl delete service asb  -n ${ASB_PROJECT}
+cluster::routes delete asb-etcd -n ${ASB_PROJECT}
+kubectl delete service etcd -n ${ASB_PROJECT}
+
 # Process required changes for local development
-oc process -f ${TEMPLATE_LOCAL_DEV} -n ${ASB_PROJECT} -p BROKER_IP_ADDR=${BROKER_IP_ADDR} -p TERMINATION=${TERMINATION} | oc create -n ${ASB_PROJECT} -f -
+cluster::process ${TEMPLATE_LOCAL_DEV} ${ASB_PROJECT} -p BROKER_IP_ADDR=${BROKER_IP_ADDR} -p TERMINATION=${TERMINATION}
 
 echo "Sleeping for a few seconds to avoid issues with broker not being able to talk to etcd."
 echo "Appears like there is a delay of when we create the asb-etcd route and when it is available for use"
@@ -166,8 +180,8 @@ if [ "$LOCAL_ETCD" == "true" ]; then
   ETCD_ROUTE="localhost"
   ETCD_PORT=2379
 else
-  ETCD_ROUTE=`oc get route asb-etcd -n ${ASB_PROJECT} -o=jsonpath=\'\{.spec.host\}\'`
-  ETCD_PORT=80
+  ETCD_ROUTE=`cluster::routes get asb-etcd -n ${ASB_PROJECT} -o=jsonpath=\'\{.spec.host\}\'`
+  ETCD_PORT="$(cluster::etcd-port)"
 fi
 
 echo "etcd route is at: ${ETCD_ROUTE}"
@@ -210,7 +224,7 @@ log:
   level: debug
   color: true
 openshift:
-  host: ${OPENSHIFT_SERVER_HOST}
+  host: ${CLUSTER_HOST}
   bearer_token_file:${BEARER_TOKEN_FILE}
   ca_file:${CA_FILE}
   image_pull_policy: ${IMAGE_PULL_POLICY}
