bd9395f7e64c5b970bee27aa6e6857d32583f360
Ryan Hallisey <rhallise@redhat.com>
2017-11-29 09:23:31 -0500

    Add destroysandbox to the fake provider

--- pkg/runtime/runtime_test.go
+++ pkg/runtime/runtime_test.go
@@ -6,3 +6,8 @@ func (f fakeProvider) CreateSandbox(podName string, namespace string, targets []
 	//TODO: Write tests for CreateSandbox using the fake kubernetes client
 	return "", nil
 }
+
+func (f fakeProvider) DestroySandbox(podName string, namespace string, targets []string, configNamespace string, keepNamespace bool, keepNamespaceOnError bool) {
+	//TODO: Write tests for DestroySandbox using the fake kubernetes client
+	return
+}
