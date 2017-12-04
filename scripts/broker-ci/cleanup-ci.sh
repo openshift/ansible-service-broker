4ff4c4026340f734247b5fab0db889890be7f8ac
Jason Montleon <jmontleo@redhat.com>
2017-12-04 09:59:53 -0500

    docs and ci template updates for apb name changes

--- scripts/broker-ci/cleanup-ci.sh
+++ scripts/broker-ci/cleanup-ci.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-oc delete -f ./templates/postgresql-mediawiki123-bind.yaml
+oc delete -f ./templates/postgresql-mediawiki-bind.yaml
 ./scripts/broker-ci/wait-for-resource.sh delete ServiceBinding mediawiki-postgresql-binding
-oc delete -f ./templates/mediawiki123.yaml
+oc delete -f ./templates/mediawiki.yaml
 oc delete -f ./templates/postgresql.yaml
-oc delete dc postgresql mediawiki123 -n default
+oc delete dc postgresql mediawiki -n default
 ./scripts/broker-ci/wait-for-resource.sh delete pod postgresql
 ./scripts/broker-ci/wait-for-resource.sh delete pod mediawiki
