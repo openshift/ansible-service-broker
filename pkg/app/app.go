fd664fcc49887e87c71c3d1352ae2fdfd578622c
jesus m. rodriguez <jmrodri@gmail.com>
2018-01-10 16:11:22 -0500

    enable unbinding subscriber

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -239,6 +239,13 @@ func CreateApp() App {
 		log.Errorf("Failed to attach subscriber to WorkEngine: %s", err.Error())
 		os.Exit(1)
 	}
+	err = app.engine.AttachSubscriber(
+		broker.NewUnbindingWorkSubscriber(app.dao),
+		broker.UnbindingTopic)
+	if err != nil {
+		log.Errorf("Failed to attach subscriber to WorkEngine: %s", err.Error())
+		os.Exit(1)
+	}
 	log.Debugf("Active work engine topics: %+v", app.engine.GetActiveTopics())
 
 	apb.InitializeSecretsCache(app.config.GetSubConfig("secrets"))
