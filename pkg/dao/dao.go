bf419ad059f4d32b5e088c1cedefd71321270192
Shawn Hurley <smhurley00@gmail.com>
2018-06-14 15:23:29 -0400

    adding unit tests for broker package (#983)
    * adding unit tests for broker
    
    * removing log statements

--- pkg/dao/dao.go
+++ pkg/dao/dao.go
@@ -32,6 +32,8 @@ func NewDao(c *config.Config) (Dao, error) {
 
 }
 
+//go:generate mockery -name=Dao -case=underscore -inpkg -note=Generated
+
 // Dao - object to interface with the data store.
 //go:generate mockery -name=Dao -case=underscore -inpkg -note=Generated
 type Dao interface {
