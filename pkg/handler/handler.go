cc3710436ec1ecbf3b4699bace58a888f04cab4f
Alaykumar Patel <alay1431@gmail.com>
2018-07-13 13:17:56 -0400

    Update ASB Bootstrap handler, make bootstrap async

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -198,8 +198,13 @@ func NewHandler(b broker.Broker, brokerConfig *config.Config, prefix string,
 func (h handler) bootstrap(w http.ResponseWriter, r *http.Request, params map[string]string) {
 	defer r.Body.Close()
 	h.printRequest(r)
-	resp, err := h.broker.Bootstrap()
-	writeDefaultResponse(w, http.StatusOK, resp, err)
+	go func() {
+		h.broker.Bootstrap()
+	}()
+	resp := map[string]string{
+		"msg": "Bootstrap job started",
+	}
+	writeDefaultResponse(w, http.StatusOK, resp, nil)
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
