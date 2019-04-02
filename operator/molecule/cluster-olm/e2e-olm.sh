9dc89e4dc4d3e5e72d9fd34f9ba5298cd97cde13
Shawn Hurley <smhurley00@gmail.com>
2019-04-02 14:59:30 -0400

    Adding ability to deploy ASB with OLM and csv to test.
    Fixing csv version and adding correct access to monitor service monitor
    
    fixing csv

---
+++ operator/molecule/cluster-olm/e2e-olm.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+DIR=$(cd $(dirname "$0")/../../deploy/olm-catalog/openshift-ansible-service-broker-manifests && pwd)
+VERSION=${VERSION:-4.1.0}
+
+NAME=${NAME:-openshift-ansible-broker-operator}
+NAMEDISPLAY=${NAME:-"OpenShift Ansible Broker Operator"}
+
+indent() {
+  INDENT="      "
+  sed "s/^/$INDENT/" | sed "s/^${INDENT}\($1\)/${INDENT:0:-2}- \1/"
+}
+
+CRD=$(cat $(ls $DIR/$VERSION/*crd.yaml) | grep -v -- "---" | indent apiVersion)
+CSV=$(cat $(ls $DIR/$VERSION/*version.yaml) | grep -v -- "---" |  indent apiVersion)
+PKG=$(cat $(ls $DIR/*package.yaml) | indent packageName)
+
+cat <<EOF | sed 's/^  *$//'
+kind: ConfigMap
+apiVersion: v1
+metadata:
+  name: $NAME
+data:
+  customResourceDefinitions: |-
+$CRD
+  clusterServiceVersions: |-
+$CSV
+  packages: |-
+$PKG
+EOF
