fdf0b687796a1c1deeff51978315761fa87fea59
Jesus Rodriguez <jmrodri@gmail.com>
2019-05-14 14:31:31 -0400

    Bug 1213 - Link to last known version of install.yaml (#1218)

--- scripts/run_latest_build.sh
+++ scripts/run_latest_build.sh
@@ -5,4 +5,5 @@ echo "                RUN LATEST BUILD IS NO LONGER SUPPORTED"
 echo "========================================================================"
 echo ""
 echo "To install the broker, please use our apb/install.yaml. For example:"
-echo "   curl https://raw.githubusercontent.com/openshift/ansible-service-broker/master/apb/install.yaml | kubectl create -f -"
+echo "   curl https://raw.githubusercontent.com/openshift/ansible-service-broker/2110c23c28fa391122a17f4dcc7022819dc1aa40/apb/install.yaml | kubectl create -f -"
+
