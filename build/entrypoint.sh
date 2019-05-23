75fa98f751d1b9464f228bb4693b3e7798c4a2c5
Shawn Hurley <smhurley00@gmail.com>
2019-05-23 10:48:35 -0400

    adding UBI dockerfile
    * creates broker/dashboard-redirector binaries
    * adds them to the image
    * NOTE: need to update the config to default to /tmp/asb.log because we
    don't have permissions to /var/log

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-USER_ID=$(id -u)
-if [ ${USER_UID} != ${USER_ID} ]; then
-  sed "s@${USER_NAME}:x:\${USER_ID}:@${USER_NAME}:x:${USER_ID}:@g" ${BASE_DIR}/etc/passwd.template > /etc/passwd
-fi
-
 if [[ -z "$BROKER_CONFIG" ]] ; then
   echo "Broker Config environment variable not set"
   exit 1
