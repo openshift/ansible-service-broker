2600d63213e3a92b34dd2a5bbd51f5c75467b088
Camila Macedo <cmacedo@redhat.com>
2020-03-01 13:35:32 +0000

    fix CVE-2019-19350 -  /etc/passwd is given incorrect privileges for molecule

--- operator/build/entrypoint.sh
+++ operator/build/entrypoint.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-if ! whoami &>/dev/null; then
-  if [ -w /etc/passwd ]; then
-    echo "${USER_NAME:-molecule}:x:$(id -u):$(id -g):${USER_NAME:-molecule} user:${HOME}:/sbin/nologin" >> /etc/passwd
-  fi
-fi
-
 exec $@
